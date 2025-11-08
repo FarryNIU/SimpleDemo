85cb8c4a4605a8edd500b21e3ce6a921f4aabd01
FrrayNIU <997643118@qq.com>
2025-11-08 12:32:23 +0800

    feat:mybatis config

--- src/main/java/com/bamboo/firstdemo/FirstdemoApplication.java
+++ src/main/java/com/bamboo/firstdemo/FirstdemoApplication.java
@@ -1,9 +1,13 @@
 package com.bamboo.firstdemo;
 
+import com.bamboo.firstdemo.dao.mapper.UserMapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bamboo.firstdemo.dao.mapper")
 public class FirstdemoApplication {
 
     public static void main(String[] args) {
