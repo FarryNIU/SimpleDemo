4347d7ac06e40e3badc5ab795a59154c72e391d0
FrrayNIU <997643118@qq.com>
2025-12-01 00:40:03 +0800

    feat:增加redis连接

---
+++ src/main/java/com/bamboo/firstdemo/config/GolbalConstant.java
@@ -0,0 +1,8 @@
+package com.bamboo.firstdemo.config;
+
+/**
+ * @author FarryNiu 2025/11/30
+ */
+public class GolbalConstant {
+    public static final String SHOP_STATUS_KEY = "sky:shop:status";
+}
